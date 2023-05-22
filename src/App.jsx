87170f3def665970af3e4fa9f7f9d4053786270c
hansololiviakim <dev.hansolkim@gmail.com>
2023-05-22 15:11:54 +0900

    [init] CRA 프로젝트 세팅 및 공통 라이브러리 설치

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Main } from './pages/index';
+
+const queryClient = new QueryClient();
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  )
+}
+
+export default App
---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { styled } from 'styled-components'
+
+const Layout = () => {
+  return (
+    <div>Layout</div>
+  )
+}
+
+export default Layout
