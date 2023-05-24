d9a6fa4f42bf7cfd428cf4965914f6d55dbf3aa4
hansololiviakim <dev.hansolkim@gmail.com>
2023-05-24 09:16:20 +0900

    [design] Navbar, Layout 컴포넌트 UI 추가

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import { styled } from 'styled-components'
+import React from 'react';
+import { styled } from 'styled-components';
+import { Navbar } from '../element'
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
-    <div>Layout</div>
+    <BackgroundDiv>
+      <ContainerDiv>
+        <WrapMain>
+          {children}
+        </WrapMain>
+        <Navbar />
+      </ContainerDiv>
+    </BackgroundDiv>
   )
 }
 
-export default Layout
+export default Layout;
+
+const BackgroundDiv = styled.div`
+  // 배경화면 색상 임시 지정
+  background-color: #F9F5F4;
+`
+
+const ContainerDiv = styled.div`
+  width: 360px;
+  height: 100vh;
+  margin: auto;
+  position: relative;
+  overflow-y: scroll;
+  background-color: #FFFFFF;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  &::-webkit-scrollbar{
+    width: 2px;
+  }
+  &::-webkit-scrollbar-thumb{
+    background-color:#CCC;
+  }
+`
+
+const WrapMain = styled.main`
+  padding: 0 16px;
+`
