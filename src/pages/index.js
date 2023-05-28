5ee23b16d4c7b45b69db42f20c2dc5612bad11a0
hansololiviakim <dev.hansolkim@gmail.com>
2023-05-28 18:12:53 +0900

    [design] 위스키바 상세조회 UI 추가 (#15)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import Error from './statusPage/Error';
 import WhiskyList from './WhiskyList';
 import WhiskyDetail from './WhiskyDetail';
+import BarDetail from './BarDetail';
 import Login from './userPage/Login';
 import Signup from './userPage/Signup';
 import StoreManagePage from './storePage/StoreManagePage';
 
-export { Error, WhiskyList, WhiskyDetail, Login, Signup, StoreManagePage };
+export { Error, WhiskyList, WhiskyDetail, BarDetail, Login, Signup, StoreManagePage };
