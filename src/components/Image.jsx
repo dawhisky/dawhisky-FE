3fc9f6b8f41f598f835d990a2dd987a5026bf374
hansololiviakim <dev.hansolkim@gmail.com>
2023-06-08 01:26:16 +0900

    [feat] Image 컴포넌트 default 이미지 추가

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -7,7 +7,19 @@ import { styled } from 'styled-components';
 // * alt : 이미지에 대한 설명으로, 이미지 아이템의 이름값을 넣어줘도 됨 (해당 가게 명이나 해당 위스키 이름)
 
 const Image = ({ width, height, borderradius, src, alt }) => {
-  return <SetImages width={width} height={height} borderradius={borderradius} src={src} alt={alt} loading="lazy" />;
+  const defaultSrc =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBHOW80iQrY2D3N296kfy1IMWOvgLF-zs6DFZLSE7ztNuXXwdHiJJdvhRu24K7ImhxH9Q&usqp=CAU';
+
+  return (
+    <SetImages
+      width={width}
+      height={height}
+      borderradius={borderradius}
+      src={src || defaultSrc}
+      alt={alt}
+      loading="lazy"
+    />
+  );
 };
 
 const SetImages = styled.img`
