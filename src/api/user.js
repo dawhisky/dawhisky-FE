727ad6b25cd05db8a4712984ae1248ca70a8f8b4
hansololiviakim <dev.hansolkim@gmail.com>
2023-06-09 16:42:51 +0900

    [feat] 위스키 코멘트 등록/수정/삭제 기능 추가 (#16)

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,7 @@
 import api from './interceptor';
 
-// 유저정보조회api, method : get, url : /api/mypage/user
-const getUserInfo = () => {
+// * 유저정보조회 api, method : get, url : /api/mypage/user
+export const getUserInfo = () => {
   return api
     .get(`/api/mypage/user`)
     .then((response) => response.data)
@@ -10,4 +10,45 @@ const getUserInfo = () => {
     });
 };
 
-export default getUserInfo;
+// * 유저가 작성한 위스키 코멘트 조회
+export const getUserComment = (id) => {
+  return api
+    .get(`/api/review/${id}`)
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
+
+// * 유저가 작성한 위스키 코멘트 등록
+export const setUserComment = ({ id, content }) => {
+  console.log(content);
+  return api
+    .post(`/api/review/${id}`, content)
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
+
+// * 유저가 작성한 위스키 코멘트 수정
+export const setEditUserComment = (comment) => {
+  const content = { content: comment.content };
+  const { id } = comment;
+  return api
+    .put(`/api/review/${id}`, content)
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
+
+// * 유저가 작성한 위스키 코멘트 삭제
+export const setDeleteUserComment = (id) => {
+  return api
+    .delete(`/api/review/${id}`)
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
