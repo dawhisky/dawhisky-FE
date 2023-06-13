eaa3a912247cb206088daae76238f8c3f9abb094
hansololiviakim <dev.hansolkim@gmail.com>
2023-06-13 15:13:17 +0900

    [feat] reset.css 삭제, SC GlobalStyle 및 ThemeProvider 적용

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './reset.css';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle, theme } from './style';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <App />
+  </ThemeProvider>,
+);
 
 reportWebVitals();
---
+++ src/style/theme.js
@@ -0,0 +1,20 @@
+const theme = {
+  colors: {
+    white: '#FFFFFF',
+    lightOrange: '#FFF6EC',
+    mediumOrange: '#EDCDA7',
+    orange: '#FF8B00',
+    lightBrown: '#7B3400',
+    darkBrown: '#2D1800',
+    lightGray: '#EAEAEA',
+    gray: '#BBBBBB',
+    darkGray: '#8F8F8F',
+  },
+
+  background: {
+    mainBackground: 'linear-gradient(#2C1900, #001E17)',
+    header: 'linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.1))',
+  },
+};
+
+export default theme;
