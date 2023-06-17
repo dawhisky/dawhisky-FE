2e2a54a5a860d6d86238ab7999e4727377accd78
allongsio <choisio2301@gmail.com>
2023-06-17 09:35:49 +0900

    [feat]firebase 토큰추출, push알림 구현

--- src/index.js
+++ src/index.js
@@ -2,22 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { initializeApp } from 'firebase/app';
-import { getMessaging } from 'firebase/messaging';
+import { getMessaging } from 'firebase/messaging/sw';
+import { getToken, onMessage } from 'firebase/messaging';
 import { GlobalStyle, theme } from './style';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <App />
-  </ThemeProvider>,
-);
-
 const firebaseConfig = {
   apiKey: 'AIzaSyDNjGs_B7jtd-cMVORaQ0Jr7de1XTr5TdE',
   authDomain: 'da-whisky.firebaseapp.com',
+  databaseURL: 'https://da-whisky-default-rtdb.firebaseio.com',
   projectId: 'da-whisky',
   storageBucket: 'da-whisky.appspot.com',
   messagingSenderId: '700714522444',
@@ -28,5 +22,36 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const messaging = getMessaging(app);
+
+getToken(messaging, {
+  vapidKey: 'BHurO6CO7I5aITSEDN9qfNy3FzJN41n-EIzJFKRWTPK2GC28hiFMEeBXxhgZjwYp92_dL4pjIZpymrvNufrmgEc',
+}).then((token) => console.log(token));
+
+onMessage(messaging, (payload) => {
+  console.log('push message: ', payload);
+});
+
+function requestPermission() {
+  console.log('권한 요청 중...');
+  Notification.requestPermission().then((permission) => {
+    if (permission === 'granted') {
+      console.log('알림 권한이 허용됨');
+
+      // FCM 메세지 처리
+    } else {
+      console.log('알림 권한 허용 안됨');
+    }
+  });
+}
+
+requestPermission();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <App />
+  </ThemeProvider>,
+);
 
 reportWebVitals();
