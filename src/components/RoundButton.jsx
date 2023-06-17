a1cef37b3ef1d605d582068ba4fd8f79828801a3
hansololiviakim <dev.hansolkim@gmail.com>
2023-06-18 05:04:20 +0900

    [feat] 클릭 시 코멘트 작성 페이지로 이동

--- src/components/RoundButton.jsx
+++ src/components/RoundButton.jsx
@@ -1,24 +1,46 @@
 import React from 'react';
 import { styled } from 'styled-components';
+import { useNavigate, useParams } from 'react-router-dom';
 import Image from './Image';
 import { edit, defaultImage } from '../assets';
 
 const RoundButton = ({ rest }) => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const onWriteButtonClickHandler = () => navigate(`/MyComment/${id}`);
+
   return (
-    <RoundedButton>
-      <Image width={'1.563rem'} height={'1.563rem'} src={edit || defaultImage} alt={'작성 버튼'} {...rest} />
-    </RoundedButton>
+    <NullDiv>
+      <RoundedButton>
+        <Image
+          width={'1.563rem'}
+          height={'1.563rem'}
+          src={edit || defaultImage}
+          alt={'작성 버튼'}
+          onClick={onWriteButtonClickHandler}
+          {...rest}
+        />
+      </RoundedButton>
+    </NullDiv>
   );
 };
 
 export default RoundButton;
 
+const NullDiv = styled.div`
+  width: 20rem;
+  height: 1.25rem;
+  position: relative;
+  display: flex;
+  justify-content: flex-end;
+`;
+
 const RoundedButton = styled.button`
   width: 3.125rem;
   height: 3.125rem;
   position: fixed;
-  bottom: 5rem;
-  right: 6.875rem;
+  bottom: 80px;
   border-radius: 50%;
   background-color: ${({ theme }) => theme.colors.orange};
   cursor: pointer;
