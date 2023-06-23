8ef4a1666ba8e2f5ffe9c2e5e7c188d0aae16f98
hansololiviakim <dev.hansolkim@gmail.com>
2023-06-24 01:35:17 +0900

    [design] background logo 반응형 media 추가, 클릭 이벤트 추가 (#157)

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components';
-import { bgFram } from '../assets';
+import { bgFram, bgLogo } from '../assets';
 import Navbar from './Navbar';
 
 const Layout = ({ children }) => {
+  const navigate = useNavigate();
+
   return (
     <BackgroundDiv>
+      <LogoImg src={bgLogo} alt={'DA WHISKY 로고'} onClick={() => navigate(`/`)} />
       <ContainerDiv>
         <WrapMain>{children}</WrapMain>
         <Navbar />
@@ -17,9 +21,34 @@ const Layout = ({ children }) => {
 export default Layout;
 
 const BackgroundDiv = styled.div`
+  height: ${window.innerHeight}px;
   background-image: url(${bgFram});
-  background-repeat: 'no-repeat';
-  background-size: 'cover';
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+  @media (max-width: 1250px) {
+    & > img {
+      width: 235px;
+    }
+  }
+  @media (max-width: 1050px) {
+    & > img {
+      width: 190px;
+    }
+  }
+  @media (max-width: 900px) {
+    & > img {
+      display: none;
+    }
+  }
+`;
+
+const LogoImg = styled.img`
+  width: 335px;
+  position: absolute;
+  top: 45px;
+  left: 60px;
+  cursor: pointer;
 `;
 
 const ContainerDiv = styled.div`
